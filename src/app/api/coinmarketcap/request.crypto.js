09a8f49236ef969c66094ed6a2346293cb3db8c1
Diego Maroto <diego.diemg@gmail.com>
2020-07-02 18:36:52 +0200

    First get cryptos api layer and first DDD crypto logic

--- src/app/api/coinmarketcap/request.crypto.js
+++ src/app/api/coinmarketcap/request.crypto.js
@@ -1,33 +1,15 @@
-/* Example in Node.js ES6 using request-promise */
-
-import fetch from 'node-fetch';
-
-let url = 'https://web-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical?';
-const params = {
-  id: '1',
-  convert: 'USD',
-  format: 'chart_crypto_details',
-}
-Object.keys(params).forEach(key => {
-  url += `${key}=${params[key]}&`;
-});
-url = url.substring(0, url.length-1);
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-CMC_PRO_API_KEY': 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-    'Content-Type': 'application/json'
+export class GetCryptos {
+  constructor(router, path) {
+    this.router = router;
+    this.path = path;
   }
-};
 
-fetch(url, options)
-  .then(response => {
-    return response.json();
-  })
-  .then((body) => {
-    console.log("RESPONSE", body);
-  })
-  .catch((err) => {
-    console.log('API call error:', err.message);
-  });
+  configure(method) {
+    this.router[method](this.path, this.handler.bind(this));
+  }
+
+  handler(request, response) {
+    const domainGetCryptosResolver = {name: 'GetCryptos'};
+    response.send(domainGetCryptosResolver);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,21 +3,26 @@ import swaggerDocument from './swagger/swagger.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import {GetCryptos} from "./app/api/coinmarketcap/request.crypto.js";
 
 const app = express();
 const router = express.Router();
 router.use(cors());
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({extended: true}));
 
 // SWAGGER CONFIG
 router.use('/swagger', swaggerUi.serve);
 router.get('/swagger', swaggerUi.setup(swaggerDocument));
 
-app.get('/', function (req, res) {
-  res.send('Esta es la prueba técnica de Bit2me');
+router.get('/', function (req, res) {
+  res.send({msg: 'Esta es la prueba técnica de Bit2me'});
 });
 
+const getCryptos = new GetCryptos(router, '/cryptos');
+getCryptos.configure('get');
+
+app.use('/', router);
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
