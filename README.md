bb0fa492f27fd0fe35c538a274ac574891ca80a4
Diego Maroto <diego.diemg@gmail.com>
2020-07-02 18:14:59 +0200

    IDE gitignore

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ Prueba técnica de Bit2me
 ## Stack
 
 Se pretende afrontar la prueba con:
-- Node v13.7
+- Node v14.5
 - Swagger resuelto desde paquetería en /docs
 - Persistencia en mongo y redis
 - Mensajería por email
 - Websockets
 - Testing con mocha
+
+## External api
+
+- coinmarketcap
+  - https://coinmarketcap.com/api/documentation/v1/
+
+### Misc
+
+Se usan @types/package para facilitar el autocompletado del IDE.
+- Instalados como dev dependency 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello world")
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger/swagger.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const router = express.Router();
+router.use(cors());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// SWAGGER CONFIG
+router.use('/swagger', swaggerUi.serve);
+router.get('/swagger', swaggerUi.setup(swaggerDocument));
+
+app.get('/', function (req, res) {
+  res.send('Esta es la prueba técnica de Bit2me');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
